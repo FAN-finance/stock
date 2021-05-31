007245c8aed2b5d5805869b4d3ae0413cb6a58d9
wuxiaofei <wxf4150@163.com>
2021-05-31 18:00:13 +0800

    币价换算

--- controls/stock.go
+++ controls/stock.go
@@ -40,11 +40,11 @@ func CoinPriceHandler(c *gin.Context) {
 	timestamp,_:=strconv.Atoi(timestampstr)
 	//err := utils.Orm.Where("code= ? and timestamp>= ? ", code,timestamp).Order("timestamp").First(info).Error
 
-	coinField:=fmt.Sprintf("cast(%s as decimal(10,2))  as coin",coin)
+	coinField:=fmt.Sprintf("cast(%s as decimal)  as coin",coin)
 	if coin=="btc"{
 		coinField="1 as coin"
 	}
-	vsCoinField:=fmt.Sprintf("cast(%s as decimal(10,2))  as vs_coin",vsCoin)
+	vsCoinField:=fmt.Sprintf("cast(%s as decimal)  as vs_coin",vsCoin)
 	if vsCoin=="btc"{
 		vsCoinField="1 as vs_coin"
 	}
