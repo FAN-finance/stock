bd0a80ba4ce5ff9e03cd4bce7c120be16938e23b
wuxiaofei <wxf4150@163.com>
2021-07-03 11:41:34 +0800

    修改14点调仓时的时间判断字段

--- services/ftx.go
+++ services/ftx.go
@@ -162,7 +162,7 @@ func SetBullsFromID(lastBullTime int64, coinType string) (int64, error) {
 
 		// 每天14点，检测是否在过去24小时之内触发过调仓
 		now := time.Now()
-		if now.Hour() == 14 && now.Minute() < 3 {
+		if now.Hour() == 14 && now.Minute() < 20 {
 			ltime:=time.Unix(LastBullAJ[coinType].Timestamp,0)
 			lastRebalanceTime := ltime
 			if now.Sub(lastRebalanceTime).Hours() >= 24 {
