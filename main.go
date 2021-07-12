62a06fd4ccec998000c51a50ef49d001c5f28f6c
wuxiaofei <wxf4150@163.com>
2021-07-12 15:17:17 +0800

    stop bull for Metaverse Index data load

--- main.go
+++ main.go
@@ -93,17 +93,17 @@ func main() {
 		//subcribe twelvedata data
 		go services.SubTwData()
 
-		//更新twelvedata数据源bull数据
-		go services.SetAllBullsFromTw()
-
-		//股票时间间隔价格统计
-		go services.SetStockStat()
+		////更新twelvedata数据源bull数据
+		//go services.SetAllBullsFromTw()
+		//
+		////股票时间间隔价格统计
+		//go services.SetStockStat()
+		//
+		////订阅coinmarketcap数据 Metaverse Index
+		//go services.SubCM()
 
 		// token totalSupply daily data
 		go services.TokenTotalSupplyDailyData()
-
-		//订阅coinmarketcap数据 Metaverse Index
-		go services.SubCM()
 	}
 
 	services.InitNodeKey()
