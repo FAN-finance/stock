8a840f70d6082868641acb2bcb85dc3b72a1e1fd
wuxiaofei <wxf4150@163.com>
2021-10-21 00:13:50 +0800

    update FtxDataNew to 5 minite

--- controls/dex.go
+++ controls/dex.go
@@ -731,7 +731,7 @@ func IsSignAble(code string, price float64) (signAble bool, msg string) {
 			return
 		}
 
-		delay:=180
+		delay:=240
 		if strings.HasPrefix(coinType,"govt"){
 			delay=600
 		}
